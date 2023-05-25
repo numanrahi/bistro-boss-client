cecd4a98ba948262c108e45c04676193b97a7c21
numanrahi <arahi2994@gmail.com>
2023-05-26 01:03:31 +0600

    menu category added

--- src/pages/Shared/Cover/Cover.jsx
+++ src/pages/Shared/Cover/Cover.jsx
@@ -9,7 +9,6 @@ const Cover = ({ img, title }) => {
             bgImageAlt="the menu"
             strength={-200}
         >
-            Blur transition from min to max
             <div className="hero h-[600px]">
                 <div className="hero-overlay bg-opacity-60"></div>
                 <div className="hero-content text-center text-neutral-content">
