f1cd73b8d56f6dd4ce53eb279845067a0af52a35
numanrahi <arahi2994@gmail.com>
2023-05-27 19:36:13 +0600

    foodCard , menuCategory, orderTab added

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -5,6 +5,7 @@ import Main from "../Layout/Main";
 import Home from "../pages/Home/Home/Home";
 import Menu from "../pages/Menu/Menu/Menu";
 import Order from "../pages/Order/Order/Order";
+import Login from "../pages/Login/Login";
 
 export const router = createBrowserRouter([
     {
@@ -16,12 +17,16 @@ export const router = createBrowserRouter([
                 element: <Home></Home>
             },
             {
-                path: '/menu',
+                path: 'menu',
                 element: <Menu></Menu>
             },
             {
-                path: '/order',
+                path: 'order/:category',
                 element: <Order></Order>
+            },
+            {
+                path: 'login',
+                element: <Login></Login>
             }
         ]
     },
